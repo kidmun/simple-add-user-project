b1f74e3da8f18f40af0163334246479e566340a1
Kidus Guade <kidusguade41@gmail.com>
2022-04-17 22:57:27 -0700

    add re-usable buttton ui

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from '../UI/Card';
-import classes from './AddUser.module.css'
-
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -16,7 +16,7 @@ return(
     <label htmlFor="age">Age (Years)</label>
     <input id="age" type="number"/>
 
-    <button type="submit">Add User</button>
+    <Button type="submit">Add User</Button>
     </form>
     </Card>
 );
