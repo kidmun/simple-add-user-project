124ebeb6e75849300bc54b42fc962eedb51b03d6
Kidus Guade <kidusguade41@gmail.com>
2022-04-17 22:33:37 -0700

    add re-usable user card component with their styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,14 @@
-import react from 'react';
+import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
 
-const AddUser = props => {
+
+const AddUser = (props) => {
     const addUserHandler = (event) => {
   event.preventDefault();
     }
 return(
+    <Card className={classes.input}>
 <form onSubmit={addUserHandler}>
     <label htmlFor="username">username</label>
     <input id="username" type="text"/>
@@ -14,6 +18,7 @@ return(
 
     <button type="submit">Add User</button>
     </form>
+    </Card>
 );
 };
 
